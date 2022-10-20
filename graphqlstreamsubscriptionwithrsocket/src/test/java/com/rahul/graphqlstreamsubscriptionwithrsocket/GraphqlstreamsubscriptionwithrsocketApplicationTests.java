23c18224ef1f0b64cd00ad53bdb6861cb65b07d9
rahulnijhawan13@gmail.com <rahulnijhawan13@gmail.com>
2022-10-20 14:05:32 +0530

    graphql subscription with rsocket

---
+++ graphqlstreamsubscriptionwithrsocket/src/test/java/com/rahul/graphqlstreamsubscriptionwithrsocket/GraphqlstreamsubscriptionwithrsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.graphqlstreamsubscriptionwithrsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlstreamsubscriptionwithrsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
